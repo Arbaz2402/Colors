3d410b86aa495568faaa763ed5a7e8008ae9ad49
Arbaz Kaladiya <kaladiya.arbaz@gmail.com>
2025-08-03 16:12:01 +0530

    Initial Commit

---
+++ Colors/ColorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorsApp.swift
+//  Colors
+//
+//  Created by Arbaz Kaladiya on 03/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ColorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
